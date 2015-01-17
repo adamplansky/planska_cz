ce6f8ac7ba8100e3d393430369991bee10309305
Adam Plansky <adamplansky@gmail.com>
2015-01-17 20:20:10 +0100

    touchstart

--- app/assets/javascripts/top_nav.js
+++ app/assets/javascripts/top_nav.js
@@ -1,16 +1,13 @@
 $(document).ready(function(){
   
-
-  
-  
-  $(document).on('click', '.link', function () {
-      $(this).parent().next(".content").toggle();
-  });
+   $(document).on('click touchstart', '.link', function () {
+       $(this).parent().next(".content").toggle();
+   });
 });
 var ready;
 ready = function() {
   $('.content').hide();
-  var url = window.location.href; 
+  var url = window.location.href;
   var uriId = url.split("#")[1];
   if(uriId != null){
     $("#" + uriId).next(".content").toggle();
