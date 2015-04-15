373a414d9294e79d8472bdda516f20c2db5e2251
Adam Plansky <adamplansky@gmail.com>
2015-04-15 10:29:05 +0200

    index links fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,10 +6,10 @@ class PagesController < ApplicationController
     @html_act = Nokogiri::HTML(act, nil, 'utf-8')
     act.close
    
-    x2013 = File.join(Rails.root, 'app', 'views', 'pages', '2013.html.erb')
-    x2013 = File.open(x2013)
-    @html_2013 = Nokogiri::HTML(x2013, nil, 'utf-8')
-    x2013.close
+    x2014 = File.join(Rails.root, 'app', 'views', 'pages', '2014.html.erb')
+    x2014 = File.open(x2014)
+    @html_2014 = Nokogiri::HTML(x2014, nil, 'utf-8')
+    x2014.close
    
     archiv = File.join(Rails.root, 'app', 'views', 'pages', 'archiv.html.erb')
     archiv = File.open(archiv)
