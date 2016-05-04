22fcb9c62dca562614932b6543796791d584266b
Adam Plansky <adamplansky@gmail.com>
2016-05-04 09:41:00 +0200

    Zvýšení daňového zvýhodnění na děti od 5/2016

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,20 @@
 class PagesController < ApplicationController
   def index
-    
+
     act = File.join(Rails.root, 'app', 'views', 'pages', 'actually.html.erb')
     act = File.open(act)
     @html_act = Nokogiri::HTML(act, nil, 'utf-8')
     act.close
-   
+
     x2014 = File.join(Rails.root, 'app', 'views', 'pages', '2014.html.erb')
     x2014 = File.open(x2014)
     @html_2014 = Nokogiri::HTML(x2014, nil, 'utf-8')
     x2014.close
-   
+
     archiv = File.join(Rails.root, 'app', 'views', 'pages', 'archiv.html.erb')
     archiv = File.open(archiv)
     @html_archiv = Nokogiri::HTML(archiv, nil, 'utf-8')
     archiv.close
-    
+
   end
 end
