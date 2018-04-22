0facb3a66eb1fba474b74b7f735aca9f2ffa6890
Adam Plánský <adamplansky@gmail.com>
2018-04-22 15:45:11 +0200

    update to 2017

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,24 +1,23 @@
+# Contacts Controller
 class ContactsController < ApplicationController
   def new
     @contact = Contact.new
   end
-  
+
   def create
-      @contact = Contact.new(secure_params)
+    @contact = Contact.new(secure_params)
     puts @contact.inspect
     if @contact.valid?
       UserMailer.ask_email(@contact).deliver
-      flash[:notice] = "Zpráva odeslána!"
-     redirect_to :controller => 'contacts', :action => 'new'
+      flash[:notice] = 'Zpráva odeslána!'
+      redirect_to controller: 'contacts', action: 'new'
     else
-      flash[:notice] = "Zprávu se nepodařilo odeslat!"
+      flash[:notice] = 'Zprávu se nepodařilo odeslat!'
       render :new
     end
   end
-  
+
   def secure_params
     params.require(:contact).permit(:name, :email, :message)
   end
-  
 end
-
