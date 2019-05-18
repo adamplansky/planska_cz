730ecee58cb71fb9834e0836973f95c812404a27
Adam Plánský <adamplansky@gmail.com>
2019-05-18 09:07:43 +0200

    2017,2018 -> archiv, add info

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,10 +6,10 @@ class PagesController < ApplicationController
     @html_act = Nokogiri::HTML(act, nil, 'utf-8')
     act.close
 
-    x2018 = File.join(Rails.root, 'app', 'views', 'pages', '2018.html.erb')
-    x2018 = File.open(x2018)
-    @html_2018 = Nokogiri::HTML(x2018, nil, 'utf-8')
-    x2018.close
+    info = File.join(Rails.root, 'app', 'views', 'pages', 'info.html.erb')
+    info = File.open(info)
+    @html_info = Nokogiri::HTML(info, nil, 'utf-8')
+    info.close
 
     archiv = File.join(Rails.root, 'app', 'views', 'pages', 'archiv.html.erb')
     archiv = File.open(archiv)
